7fea1c970c14e08d1d2a06387c98c388133fd4dc
kostandinvllahu <Ghostsechackers@gmail.com>
2021-02-20 18:10:40 +0100

    Testing with main

---
+++ main.php
@@ -0,0 +1,23 @@
+<!Doctype html>
+<head>
+<title>Map Creator</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+<h1 align="center">WELCOME TO MAP CREATOR</h1>
+</head>
+<style>
+.map{
+	border-style:groove;
+	height: 50px;
+	width: 500px;
+	align: center;
+}
+</style>
+<body>
+<div class="map">
+<p>TEST</p>
+
+<div style="width: 100%"><iframe width="100%" height="600" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=17%20lancelot%20lane%20Henrietta%20New York+(Kosta)&amp;t=k&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe><a href="https://www.maps.ie/route-planner.htm">Road Trip Planner</a></div>
+
+</div>
+</body>
+</html>
